8f7c3465efb381ab580e2e1de6d509302f386d11
siham0417 <74072626+siham0417@users.noreply.github.com>
2022-11-28 21:19:57 +0000

    bookcase attempt

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Book from './components/Book';
 import data from './models/books.json';
 
-function App() {
-  const books = data;
-  return <Book/>;
+function App () {
+  const [books] = useState(data);
+  return <ul>
+    <h1>Book Library</h1>
+{books.map((book) => <Book key={book.id} book={book}/>)}
+     
+</ul>
 }
 
 export default App;
+
 
--- bookcase-app/src/components/Book.js
+++ bookcase-app/src/components/Book.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-function Book() {
-  return "This is the output of a book component. Replace this string with code to display an individual book's attributes.";
+function Book({book}) {
+  const { title, authors } = book.volumeInfo;
+  const { retailPrice } = book.saleInfo;
+  console.log(book)
+  return (
+  <div style={{
+    backgroundColor: "grey",
+    fontSize: "20px",
+    marginBottom: "6px"}}>
+
+    <h3>{title}</h3>
+    {retailPrice && retailPrice.amount < 3 ? "Great deal" : "Best seller"}
+  </div>
+  );
 }
+
+  Book.propTypes = {
+    book: PropTypes.shape({
+      volumeInfo: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        authors: PropTypes.array.isRequired,
+        description: PropTypes.string.isRequired,
+      }),
+      saleInfo: PropTypes.shape({
+        retailPrice: PropTypes.shape({
+          amount: PropTypes.number.isRequired,
+        }),
+      }),
+    }),
+  };
+
+ 
 
 export default Book;
