7519a8cae20b242d9a0215603efff1770f58e66a
siham0417 <74072626+siham0417@users.noreply.github.com>
2022-11-30 19:44:22 +0000

    firsttask:fix

--- nesting-and-events/src/index.js
+++ nesting-and-events/src/index.js
@@ -1,28 +1,43 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 import TaskContainer from './components/TaskContainer';
-/*
+import Task from './components/Task';
+
 const taskList = [
-  {"id":"task1","title":"Buy Milk","isComplete":true},
-  {"id":"task2","title":"Read a book","isComplete":false},
-  {"id":"task3","title":"Early morning run","isComplete":false},
-  {"id":"task4","title":"Drink water","isComplete":false},
-  {"id":"task5","title":"Visit the puppy shelter","isComplete":true},
-  {"id":"task6","title":"Watch Bad Sisters","isComplete":false}
+  { id: "task1", title: "Buy Milk", isComplete: true},
+  { id:"task2", title: "Read a book", isComplete: false},
+  { id:"task3", title: "Early morning run", isComplete:false},
+  { id:"task4", title:"Drink water", isComplete:false},
+  { id:"task5", title:"Visit the puppy shelter", isComplete:true},
+  { id:"task6", title:"Watch Bad Sisters", isComplete:false}
 ];
-*/
+
 
 function App() {
-  
+  const [data] = React.useState(taskList)
    return (
       <Fragment>
         <h1 key="heading">Task List</h1>
         <TaskContainer>
-          {/* Task: Nesting Components - Add you tasks here: */}
+          {data.map((item) => {
+            const {title, isComplete, id} = item;
+            console.log(item)
+            return <Task 
+            key={id} 
+            id={id} 
+            title={title} 
+            isComplete={isComplete}/>
+          })}
+          <Task 
+          id={taskList[0].id} 
+          title={taskList[0].title} 
+          isComplete={taskList[0].isComplete}>
+          </Task>
         </TaskContainer>
       </Fragment>
   );
 }
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
