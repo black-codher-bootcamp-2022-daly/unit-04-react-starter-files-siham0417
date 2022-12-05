f11cfd5c97d670d2bf38342b931bc9b73dce74a3
siham0417 <74072626+siham0417@users.noreply.github.com>
2022-12-05 19:36:10 +0000

    07sessionTask1

--- routes-and-forms/src/index.js
+++ routes-and-forms/src/index.js
@@ -1,5 +1,6 @@
 import {React} from "react";
 import ReactDOM from 'react-dom/client';
+import propTypes from "prop-types";
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom'
 
 export default function App() {
@@ -8,10 +9,14 @@ export default function App() {
       <ul className="breadcrumb">
         <li><Link to="/"> All Pets </Link></li>
         <li><Link to="/kittens"> Kittens </Link></li>
+        <li><Link to="/puppies"> Puppies </Link></li>
+        <li><Link to="/duckling"> Duckling </Link></li>
       </ul>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="kittens" element={<Kittens />} />
+        <Route path="kittens" element={<Animal name="kitten" img="imgs/kitten.jpg" />} />
+        <Route path="puppies" element={<Animal name="puppies" img="imgs/puppy.jpg" />} />
+        <Route path="duckling" element={<Animal name="duckling" imgs="imgs/duckling.jpg" />} />
       </Routes>
   </Router>
   );
@@ -35,5 +40,38 @@ function Kittens() {
     </>
 }
 
+function Puppies() {
+
+  return <div>
+    <h2>Puppies</h2>
+    <section>This is the dogs page</section>
+    <img src="imgs/puppy.jpg" alt="puppy"/>
+  </div>
+}
+
+function Duckling() {
+
+  return <div>
+    <h3>Ducklings</h3>
+    <section>This is the duckling page</section>
+    <img src="imgs/duckling.jpg" alt="duckling"/>
+  </div>
+}
+
+function Animal ({name, img}) {
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <section>This is the {name} page</section>
+      <img src={img} alt={name} />
+    </div>
+  )
+}
+
+Animal.propTypes = {
+  name: propTypes.string,
+  img: propTypes.string,
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
