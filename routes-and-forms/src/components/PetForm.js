78ec5be9a46aec908a3575f51975a177195b527c
siham0417 <74072626+siham0417@users.noreply.github.com>
2022-12-05 20:57:09 +0000

    07sessionupdated

--- routes-and-forms/src/components/PetForm.js
+++ routes-and-forms/src/components/PetForm.js
@@ -3,10 +3,14 @@ import React, { useState } from "react";
 const PetForm = () => {
   const [name, setName] = useState("");
   const [age, setAge] = useState(0);
-  const [, setSubmitted] = useState(false);
+  const [color, setColor] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
   function handleSubmit(event) {
     event.preventDefault();
+    setAge(0)
+    setName("")
+    setColor("")
     setSubmitted(true);
   }
 
@@ -43,13 +47,35 @@ const PetForm = () => {
           </label>
         </div>
 
+        <div>
+          <label>
+            Pet's color:
+            <input
+              type="text"
+              value={color}
+              onChange={(e) => {
+                setColor(e.target.value);
+                setSubmitted(false);
+              }}
+            />
+          </label>
+        </div>
+
         <input type="submit" value="Submit" id="submit-button" />
       </form>
-      <div id="results">
+
+<div id="results">
+      {submitted ? (
+        "Thanks for submitting"
+        )  : (
+       <>
         <h3>Results</h3>
-        Name: {name}
-        Age: {age}
-        <br />
+        <div>Name: {name}</div>
+        <div>Age: {age}</div>
+        <div>color: {color}</div>
+        </>
+        )}
+      <br />
       </div>
     </div>
   );
